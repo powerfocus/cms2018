e443e2320777b729c3927a143bd0d2fb0863ff65
magicianhy@126.com <hy1980629>
2018-12-08 20:14:47 +0800

    change
    Signed-off-by: magicianhy@126.com <hy1980629>

--- src/main/java/org/py/controller/admin/ArticleController.java
+++ src/main/java/org/py/controller/admin/ArticleController.java
@@ -2,7 +2,10 @@ package org.py.controller.admin;
 
 import lombok.extern.java.Log;
 import org.py.model.Article;
+import org.py.model.Columntype;
+import org.py.util.CategoryUtil;
 import org.py.util.TemplateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -11,7 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Log
 @Controller
@@ -20,6 +26,8 @@ public class ArticleController implements AdminBaseController {
     private final String TEMPLATECSS = "artcss";
     private final String TEMPLATESTYLE = "artstyle";
     private static final String NAV = TemplateUtil.topNav(Arrays.asList("栏目管理"));
+    @Autowired
+    private CategoryUtil categoryUtil;
     @GetMapping({"index"})
     public String index(Model model) {
         model.addAttribute(TEMPLATECSS, CSS);
@@ -29,9 +37,12 @@ public class ArticleController implements AdminBaseController {
     }
     @GetMapping({"add"})
     public String add(Article article, Model model) {
+        List<Map<String, Columntype>> list = new ArrayList<>();
+        categoryUtil.treeList(0, 0, list);
         model.addAttribute(TEMPLATECSS, CSS);
         model.addAttribute(TEMPLATESTYLE, STYLE);
         model.addAttribute("navpath", NAV);
+        model.addAttribute("list", list);
         return "admin/article-add";
     }
     @PostMapping({"add"})
