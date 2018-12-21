ae7a850a7eda345d88667c250d8e78e0bd3ba072
pythagoras <magicianhy@126.com>
2018-12-21 16:34:05 +0800

    change

---
+++ src/main/java/org/py/controller/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package org.py.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler({Exception.class})
+    public String exception(Exception e, Model model) {
+        model.addAttribute("msg", e.getMessage());
+        return "error";
+    }
+}
