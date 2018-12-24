fbca5328f56d126a9f3e96977fd5c1577a1d2820
pythagoras <magicianhy@126.com>
2018-12-24 17:27:46 +0800

    change

--- src/main/java/org/py/controller/admin/ArticleController.java
+++ src/main/java/org/py/controller/admin/ArticleController.java
@@ -13,10 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
@@ -94,6 +91,7 @@ public class ArticleController implements AdminBaseController {
         return "optSuccess";
     }
     @GetMapping({"del/{id}"})
+    @ResponseBody
     public Map<String, Object> del(@PathVariable Integer id) {
         Map<String, Object> data = new HashMap<>();
         data.put("state", "success");
